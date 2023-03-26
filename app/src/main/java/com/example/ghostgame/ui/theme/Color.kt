7501d6fa289d0ea9de8d058f9d1d921b08459f60
Daniyl Zakusilo <danzakusilo@users.noreply.github.com>
2023-03-26 14:04:33 +0300

    add game colors

--- app/src/main/java/com/example/ghostgame/ui/theme/Color.kt
+++ app/src/main/java/com/example/ghostgame/ui/theme/Color.kt
@@ -6,3 +6,14 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Green = Color(0xFF67E65C)
+val GameCellCorrect = Green
+
+val Red = Color(0xFFFD7777)
+val GameCellWrong = Red
+
+val Blue = Color(0xFF78E2F3)
+val GameCellGhost = Blue
+
+val Grey = Color(0x66000000)
